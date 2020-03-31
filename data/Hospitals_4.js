453b88ae23ed8ca1acfa3a84352081673a87e330
jcadag <62292076+jcadag@users.noreply.github.com>
2020-03-31 15:59:45 +0800

    Add files via upload

---
+++ data/Hospitals_4.js
@@ -0,0 +1,10 @@
+var json_Hospitals_4 = {
+"type": "FeatureCollection",
+"name": "Hospitals_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Name": "Provincial Hospital", "Capacity": "100 patients" }, "geometry": { "type": "Point", "coordinates": [ 118.74694613334384, 9.754648982736514 ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "Clinic", "Capacity": "20 patients" }, "geometry": { "type": "Point", "coordinates": [ 118.744428076105521, 9.748825321935017 ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "Clinic", "Capacity": "10 patients" }, "geometry": { "type": "Point", "coordinates": [ 118.744461650202041, 9.747832642333432 ] } }
+]
+}
---
+++ data/QuarantineFacilities_5.js
@@ -0,0 +1,8 @@
+var json_QuarantineFacilities_5 = {
+"type": "FeatureCollection",
+"name": "QuarantineFacilities_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Name": "CDRRMO", "Capacity": "100 persons" }, "geometry": { "type": "Point", "coordinates": [ 118.748557689976337, 9.749652552679132 ] } }
+]
+}
